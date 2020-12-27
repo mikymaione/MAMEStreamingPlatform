5484d92629dde14c817ef045aa07e42b9bc9a13e
Michele Maione <mikymaione@hotmail.it>
2020-12-27 11:09:16 +0100

    UML

---
+++ src/osd/modules/osd_stream.h
@@ -0,0 +1,12 @@
+// license:BSD-3-Clause
+// copyright-holders:Michele MAione
+//============================================================
+//
+//  osd_stream.h - MAME Streaming
+//
+//============================================================
+
+class osd_stream
+{
+    
+};
