c735fae833e423ce420dcc6fc6a53f7165156035
Michele Maione <mikymaione@hotmail.it>
2020-12-27 15:59:35 +0100

    tutto ok

--- src/osd/modules/osd_stream.cpp
+++
@@ -1,9 +0,0 @@
-// license:BSD-3-Clause
-// copyright-holders:Michele MAione
-//============================================================
-//
-//  osd_stream.cpp - MAME Streaming
-//
-//============================================================
-
-#include "osd_stream.h"
