eb92caa436633f3df0450519e4844e6191ac9386
Michele Maione <mikymaione@hotmail.it>
2021-02-13 16:44:57 +0100

    SDL compila e funziona su Windows10

--- compila vs2019 SDL.bat
+++ compila vs2019 SDL.bat
@@ -1,2 +1,2 @@
 cls
-make vs2019 PYTHON_EXECUTABLE=C:/Users/Public/msys64/mingw64/bin/python.exe OSD=sdl USE_BUNDLED_LIB_SDL2=1 TARGET=mame SOURCES=src/mame/drivers/cps3.cpp USE_BUNDLED_LIB_SDL2=0 USE_LIBSDL=1 SDL_INSTALL_ROOT=C:/SDL2/ REGENIE=1 -j3
+make vs2019 PYTHON_EXECUTABLE=C:/Users/Public/msys64/mingw64/bin/python.exe OSD=sdl TARGET=mame SOURCES=src/mame/drivers/cps3.cpp USE_BUNDLED_LIB_SDL2=0 USE_LIBSDL=1 SDL_INSTALL_ROOT=C:/SDL2/ REGENIE=1 -j3
