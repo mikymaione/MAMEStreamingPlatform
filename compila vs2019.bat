f661b392c3efea2934953c77d682c36a4b110a5a
Michele Maione <mikymaione@hotmail.it>
2021-02-13 10:39:39 +0100

    batch

--- compila vs2019.bat
+++ compila vs2019.bat
@@ -1,2 +1,2 @@
 cls
-make vs2019 PYTHON_EXECUTABLE=C:/Users/Public/msys64/mingw64/bin/python.exe TARGET=mame SOURCES=src/mame/drivers/cps3.cpp REGENIE=1 -j4
+make vs2019 PYTHON_EXECUTABLE=C:/Users/Public/msys64/mingw64/bin/python.exe TARGET=mame SOURCES=src/mame/drivers/cps3.cpp REGENIE=1 -j3
