1e4f3c76e96569c0722ae713806a64694c29f930
Michele Maione <michele.maione@fedoramiky.homenet.telecomitalia.it>
2021-03-07 12:42:39 +0100

    compilazione clang

--- compila.sh
+++ compila.sh
@@ -1,2 +1,3 @@
 clear
-make OSD=sdl TARGET=mame SOURCES=src/mame/drivers/cps3.cpp IGNORE_GIT=1 SYMBOLS=1 REGENIE=1 -j3
+env LDFLAGS=-stdlib=libc++
+make OSD=sdl TARGET=mame SOURCES=src/mame/drivers/cps3.cpp IGNORE_GIT=1 SYMBOLS=1 OVERRIDE_CC=clang OVERRIDE_CXX=clang++ ARCHOPTS_CXX=-stdlib=libc++ ARCHOPTS_OBJCXX=-stdlib=libc++ -j3
