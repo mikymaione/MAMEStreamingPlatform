3f25cf55654291baf77913cdbfcc04ea21043f4e
Michele Maione <mikymaione@hotmail.it>
2021-03-29 11:54:00 +0200

    non funziona su chrome

--- esegui.bat
+++ esegui.bat
@@ -1,4 +1,4 @@
 setx SDL_VIDEODRIVER windows
 setx SDL_AUDIODRIVER directsound
-mame64.exe -window -video accel -sound sdl
+mame64.exe -streamingserver -window -video accel -sound sdl -resolution 640x480
 pause
