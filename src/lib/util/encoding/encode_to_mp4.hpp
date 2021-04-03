6ac86ace0812cfa5fb5748b4222f681ebda6a2b4
Michele Maione <mikymaione@hotmail.it>
2021-04-03 18:06:09 +0200

    non funziona mp4

--- src/lib/util/encoding/encode_to_mp4.hpp
+++ src/lib/util/encoding/encode_to_mp4.hpp
@@ -8,8 +8,11 @@
 #pragma once
 
 #include <chrono>
+#include <functional>
+#include <iostream>
 #include <ostream>
-#include <queue>
+#include <memory>
+#include <string>
 
 // FFMPEG C headers
 extern "C"
@@ -26,11 +29,15 @@ namespace encoding
 {
 	class encode_to_mp4
 	{
+	public:
+		enum CODEC { MP4, WEBM, MPEGTS };
+		std::shared_ptr<std::ostream> socket;
+
 	private:
 		struct StreamingContext
 		{
-			AVIOContext* io_context = nullptr;
-			AVFormatContext* format_context = nullptr;
+			//AVIOContext* io_context = nullptr;
+			AVFormatContext* muxer_context = nullptr;
 
 			AVCodec* video_codec = nullptr;
 			AVCodec* audio_codec = nullptr;
@@ -51,13 +58,14 @@ namespace encoding
 		};
 
 	private:
-		static constexpr size_t MEMORY_OUTPUT_BUFFER_SIZE = 1024 * 1024 * 100; //100MB
+		static constexpr size_t MEMORY_OUTPUT_BUFFER_SIZE = 1024 * 64; //64K
 
-		const char* CONTAINER_NAME = "rawvideo";
+		CODEC codec;
+		std::string CONTAINER_NAME;
 
 		// Video
-		static constexpr AVCodecID VIDEO_CODEC = AV_CODEC_ID_H264;
-		static constexpr int64_t VIDEO_BIT_RATE = 128 * 1024;
+		AVCodecID VIDEO_CODEC;
+		static constexpr int64_t VIDEO_BIT_RATE = 96 * 1000;
 
 		//SDL_PIXELFORMAT_RGBA32 = AV_PIX_FMT_BGR32
 		//SDL_PIXELFORMAT_RGB24 = AV_PIX_FMT_RGB24
@@ -65,8 +73,8 @@ namespace encoding
 		static constexpr AVPixelFormat PIXEL_FORMAT_OUT = AV_PIX_FMT_YUV420P;
 
 		// Audio
-		static constexpr AVCodecID AUDIO_CODEC = AV_CODEC_ID_AAC;
-		static constexpr int64_t AUDIO_BIT_RATE = 96 * 1024;
+		AVCodecID AUDIO_CODEC;
+		static constexpr int64_t AUDIO_BIT_RATE = 64 * 1000;
 
 		static constexpr int AUDIO_CHANNELS_IN = 2;
 		static constexpr int AUDIO_CHANNELS_OUT = 2; //1
@@ -78,18 +86,19 @@ namespace encoding
 		static constexpr AVSampleFormat AUDIO_SAMPLE_FORMAT_OUT = AV_SAMPLE_FMT_FLTP;
 		static constexpr AVSampleFormat AUDIO_SAMPLE_FORMAT_IN = AV_SAMPLE_FMT_S16; //AUDIO_S16SYS
 
-	public:
-		std::function<int(uint8_t* buf, int buf_size)> on_write;
-
 	private:
 		int in_width, in_height, out_width, out_height;
 		const int fps;
+
+		std::function<void()> on_write;
 
 	private:
 		std::chrono::time_point<std::chrono::system_clock> start_time =
 			std::chrono::system_clock::now();
 
 		char error_buffer[AV_ERROR_MAX_STRING_SIZE];
+
+		bool header = false;
 
 		StreamingContext* encoder_context = nullptr;
 		Encoder_pts* video_encoder_pts = nullptr;
@@ -99,11 +108,10 @@ namespace encoding
 		bool got_packet_ptr = false;
 		AVPacket video_packet, audio_packet;
 
+		AVFrame* wav_frame = nullptr;
 		AVFrame* aac_frame = nullptr;
 		uint8_t* aac_buffer = nullptr;
-
-		uint8_t* audio_stream_temp = nullptr;
-		uint8_t audio_stream_temp_shift = 0;
+		uint8_t* convertedData = nullptr;
 
 		AVFrame* rgb_frame = nullptr;
 		AVFrame* yuv_frame = nullptr;
@@ -121,16 +129,19 @@ namespace encoding
 			exit(error_code);
 		}
 
-		static int write_buffer(void* opaque, uint8_t* buf, int buf_size)
+		static int write_packet(void* opaque, uint8_t* buf, int buf_size)
 		{
 			auto* const this_ = static_cast<encode_to_mp4*>(opaque);
 
-			return this_->on_write(buf, buf_size);
+			this_->socket->write(reinterpret_cast<const char*>(buf), buf_size);
+
+			return 1; // 1 element
 		}
 
 		void init_video()
 		{
-			encoder_context->video_stream = avformat_new_stream(encoder_context->format_context, nullptr);
+			encoder_context->video_stream = avformat_new_stream(encoder_context->muxer_context, nullptr);
+			encoder_context->video_stream->id = 0;
 
 			encoder_context->video_codec = avcodec_find_encoder(VIDEO_CODEC);
 			if (!encoder_context->video_codec)
@@ -140,24 +151,48 @@ namespace encoding
 			if (!encoder_context->video_codec_context)
 				die("Could not allocated memory for codec context", 0);
 
+			if (VIDEO_CODEC != AV_CODEC_ID_MPEG1VIDEO)
+			{
+				encoder_context->video_codec_context->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+				encoder_context->video_codec_context->flags2 |= AV_CODEC_FLAG2_FAST;
+			}
+
 			encoder_context->video_codec_context->width = out_width;
 			encoder_context->video_codec_context->height = out_height;
 
 			encoder_context->video_codec_context->bit_rate = VIDEO_BIT_RATE;
 
+			encoder_context->video_codec_context->max_b_frames = 0;
+			encoder_context->video_codec_context->has_b_frames = 0;
+			encoder_context->video_codec_context->delay = 0;
+			encoder_context->video_codec_context->gop_size = 10;
+
 			encoder_context->video_codec_context->pix_fmt = PIXEL_FORMAT_OUT;
+
 			encoder_context->video_codec_context->framerate = { fps ,1 };
 			encoder_context->video_codec_context->time_base = { 1,fps };
-			encoder_context->video_stream->time_base = encoder_context->video_codec_context->time_base;
+			//encoder_context->video_stream->time_base = encoder_context->video_codec_context->time_base;
 
 			AVDictionary* options = nullptr;
-			av_dict_set(&options, "preset", "ultrafast", 0);
-			av_dict_set(&options, "tune", "zerolatency", 0);
-			//av_dict_set(&options, "crf", "40", 0);
+			if (VIDEO_CODEC == AV_CODEC_ID_VP8)
+			{
+				//
+			}
+			else if (VIDEO_CODEC == AV_CODEC_ID_VP9)
+			{
+				av_dict_set(&options, "deadline", "realtime", 0);
+			}
+			else if (VIDEO_CODEC == AV_CODEC_ID_H264)
+			{
+				av_dict_set(&options, "preset", "ultrafast", 0);
+				av_dict_set(&options, "tune", "zerolatency", 0);
+			}
 
 			auto ret = avcodec_open2(encoder_context->video_codec_context, encoder_context->video_codec, &options);
 			if (ret < 0)
 				die("Could not open the codec", ret);
+
+			av_dict_free(&options);
 
 			ret = avcodec_parameters_from_context(encoder_context->video_stream->codecpar, encoder_context->video_codec_context);
 			if (ret < 0)
@@ -190,7 +225,8 @@ namespace encoding
 
 		void init_audio()
 		{
-			encoder_context->audio_stream = avformat_new_stream(encoder_context->format_context, nullptr);
+			encoder_context->audio_stream = avformat_new_stream(encoder_context->muxer_context, nullptr);
+			encoder_context->audio_stream->id = 1;
 
 			encoder_context->audio_codec = avcodec_find_encoder(AUDIO_CODEC);
 			if (!encoder_context->audio_codec)
@@ -207,15 +243,17 @@ namespace encoding
 
 			encoder_context->audio_codec_context->bit_rate = AUDIO_BIT_RATE;
 
-			encoder_context->audio_codec_context->time_base = { 1,SAMPLE_RATE_OUT };
-			encoder_context->audio_stream->time_base = encoder_context->audio_codec_context->time_base;
+			encoder_context->audio_codec_context->time_base = { 1,fps };
+			//encoder_context->audio_codec_context->time_base = { 1,SAMPLE_RATE_OUT };
+			//encoder_context->audio_stream->time_base = encoder_context->audio_codec_context->time_base;
 
 			AVDictionary* options = nullptr;
-			av_dict_set(&options, "movflags", "+faststart", 0);
 
 			auto ret = avcodec_open2(encoder_context->audio_codec_context, encoder_context->audio_codec, &options);
 			if (ret < 0)
 				die("Could not open the codec", ret);
+
+			av_dict_free(&options);
 
 			avcodec_parameters_from_context(encoder_context->audio_stream->codecpar, encoder_context->audio_codec_context);
 
@@ -225,6 +263,12 @@ namespace encoding
 			aac_frame->channel_layout = encoder_context->audio_codec_context->channel_layout;
 			aac_frame->format = encoder_context->audio_codec_context->sample_fmt;
 			aac_frame->sample_rate = encoder_context->audio_codec_context->sample_rate;
+
+			wav_frame = av_frame_alloc();
+			wav_frame->channels = AUDIO_CHANNELS_IN;
+			wav_frame->channel_layout = av_get_default_channel_layout(AUDIO_CHANNELS_IN);
+			wav_frame->format = AUDIO_SAMPLE_FORMAT_IN;
+			wav_frame->sample_rate = SAMPLE_RATE_IN;
 
 			// Resampler
 			encoder_context->audio_converter_context = swr_alloc_set_opts(
@@ -252,70 +296,122 @@ namespace encoding
 				die("Cannot alloc audio conversion buffer", ret);
 		}
 
-		void send_it()
+		void write_video_pts()
 		{
-			const auto ret = av_write_trailer(encoder_context->format_context);
-			if (ret < 0)
-				die("Error writing trailer", ret);
+			video_packet.pts = video_encoder_pts->video_pts;
+			video_packet.dts = video_packet.pts;
+			video_packet.duration = video_encoder_pts->frame_duration;
+			video_packet.stream_index = 0;
 
-			write_header();
+			video_encoder_pts->video_pts += video_encoder_pts->frame_duration;
+		}
+
+		void set_container_options(AVDictionary* options) const
+		{
+			switch (codec)
+			{
+				case MP4:
+					//av_dict_set(&options, "movflags", "empty_moov+frag_keyframe+default_base_moof+faststart", 0); //xpra
+					//av_dict_set(&options, "movflags", "empty_moov+omit_tfhd_offset+separate_moof+frag_custom", 0);
+					av_dict_set(&options, "movflags", "+frag_keyframe+omit_tfhd_offset+empty_moov+default_base_moof", 0);
+					av_dict_set(&options, "frag_duration", "700000", 0);
+					break;
+				case WEBM:
+					av_dict_set(&options, "movflags", "dash+live", 0);
+					break;
+				case MPEGTS:
+					break;
+			}
 		}
 
 		void write_header()
 		{
-			AVDictionary* options = nullptr;
+			header = true;
 
-			const auto ret = avformat_write_header(encoder_context->format_context, &options);
+			AVDictionary* options = nullptr;
+			set_container_options(options);
+
+			const auto ret = avformat_write_header(encoder_context->muxer_context, &options);
 			if (ret < 0)
 				die("Could not write header", ret);
+
+			av_dict_free(&options);
 		}
 
-		void write_video_pts()
+		void send_it()
 		{
-			video_packet.pts = video_encoder_pts->video_pts;
-			video_encoder_pts->video_pts += video_encoder_pts->frame_duration;
-			video_packet.dts = video_packet.pts;
-			video_packet.duration = video_encoder_pts->frame_duration;
-			video_packet.stream_index = 0;
+			const auto ret = av_write_trailer(encoder_context->muxer_context);
+			if (ret < 0)
+				die("Error writing trailer", ret);
+
+			on_write();
+
+			header = false;
 		}
 
 	public:
-		encode_to_mp4(const int in_width, const int in_height,
+		encode_to_mp4(CODEC codec,
+					  const int in_width, const int in_height,
 					  const int out_width, const int out_height,
-					  const int fps) :
+					  const int fps,
+					  const std::function<void()>& on_write) :
+			codec(codec),
 			in_width(in_width),
 			in_height(in_height),
 			out_width(out_width),
 			out_height(out_height),
-			fps(fps)
+			fps(fps),
+			on_write(on_write)
 		{
+			av_log_set_level(AV_LOG_DEBUG);
+
 			av_register_all();
 			avcodec_register_all();
+
+			switch (codec)
+			{
+				case MP4:
+					CONTAINER_NAME = "mp4";
+					VIDEO_CODEC = AV_CODEC_ID_H264;
+					AUDIO_CODEC = AV_CODEC_ID_AAC;
+					break;
+				case WEBM:
+					CONTAINER_NAME = "webm";
+					VIDEO_CODEC = AV_CODEC_ID_VP9;
+					AUDIO_CODEC = AV_CODEC_ID_VORBIS;
+					break;
+				case MPEGTS:
+					CONTAINER_NAME = "mpegts";
+					VIDEO_CODEC = AV_CODEC_ID_MPEG1VIDEO;
+					AUDIO_CODEC = AV_CODEC_ID_MP2;
+					break;
+			}
 
 			video_encoder_pts = new Encoder_pts();
 			encoder_context = new StreamingContext();
 
+			auto ret = avformat_alloc_output_context2(&encoder_context->muxer_context, nullptr, CONTAINER_NAME.c_str(), nullptr);
+			if (ret < 0)
+				die("Cannot allocate output context", ret);
+
 			memory_output_buffer = static_cast<uint8_t*>(av_malloc(MEMORY_OUTPUT_BUFFER_SIZE));
-			encoder_context->io_context = avio_alloc_context(memory_output_buffer, MEMORY_OUTPUT_BUFFER_SIZE, 1, this, nullptr, write_buffer, nullptr);
 
-			avformat_alloc_output_context2(&encoder_context->format_context, nullptr, CONTAINER_NAME, nullptr);
+			encoder_context->muxer_context->pb = avio_alloc_context(memory_output_buffer, MEMORY_OUTPUT_BUFFER_SIZE, 1, this, nullptr, write_packet, nullptr);
+			if (encoder_context->muxer_context->pb == nullptr)
+				die("Cannot allocate context", ret);
 
-			encoder_context->format_context->pb = encoder_context->io_context;
-			encoder_context->format_context->flags = AVFMT_FLAG_CUSTOM_IO;
-
-			//if (encoder_context->format_context->oformat->flags & AVFMT_GLOBALHEADER)
-				//encoder_context->format_context->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+			encoder_context->muxer_context->flush_packets = 1;
+			encoder_context->muxer_context->start_time = 0;
+			encoder_context->muxer_context->strict_std_compliance = 1;
 
 			init_video();
 			init_audio();
-
-			write_header();
 		}
 
 		~encode_to_mp4()
 		{
-			avformat_close_input(&encoder_context->format_context);
-			avformat_free_context(encoder_context->format_context);
+			avformat_close_input(&encoder_context->muxer_context);
+			avformat_free_context(encoder_context->muxer_context);
 
 			//Video
 			sws_freeContext(encoder_context->video_converter_context);
@@ -340,18 +436,19 @@ namespace encoding
 
 			av_freep(&aac_buffer);
 
-			delete[] audio_stream_temp;
-
 			delete encoder_context;
 			delete video_encoder_pts;
 		}
 
 		/**
 		 * \brief Add video frame
-		 * \param pixels
+		 * \param pixels input pixels
 		 */
 		void add_frame(const uint8_t* pixels)
 		{
+			if (!header)
+				write_header();
+
 			avpicture_fill(
 				reinterpret_cast<AVPicture*>(rgb_frame),
 				pixels,
@@ -374,7 +471,7 @@ namespace encoding
 			{
 				write_video_pts();
 
-				const auto ret = av_interleaved_write_frame(encoder_context->format_context, &video_packet);
+				const auto ret = av_interleaved_write_frame(encoder_context->muxer_context, &video_packet);
 				if (ret < 0)
 					die("Error while writing video frame", ret);
 
@@ -391,63 +488,95 @@ namespace encoding
 
 		/**
 		 * \brief Add audio instant
-		 * \param audio_stream
-		 * \param audio_stream_size
-		 * \param audio_stream_num_samples
+		 * \param audio_stream input audio stream
+		 * \param audio_stream_size input audio stream size
+		 * \param audio_stream_num_samples number of samples
 		 */
 		void add_instant(const uint8_t* audio_stream, const int audio_stream_size, const int audio_stream_num_samples)
 		{
-			if (audio_stream_temp == nullptr)
-				audio_stream_temp = new uint8_t[audio_stream_size * 2];
+			if (!header)
+				write_header();
 
-			for (auto i = 0; i < audio_stream_size; i++)
-				audio_stream_temp[i + audio_stream_temp_shift] = audio_stream[i];
+			wav_frame->nb_samples = audio_stream_num_samples;
 
-			audio_stream_temp_shift += audio_stream_size;
+			auto ret = avcodec_fill_audio_frame(
+				wav_frame,
+				AUDIO_CHANNELS_IN,
+				AUDIO_SAMPLE_FORMAT_IN,
+				audio_stream,
+				audio_stream_size,
+				1); //no-alignment
+			if (ret < 0)
+				die("Cannot fill audio frame", ret);
 
-			if (audio_stream_temp_shift == audio_stream_size * 2)
+			if (convertedData == nullptr)
 			{
-				audio_stream_temp_shift = 0;
+				ret = av_samples_alloc(
+					&convertedData,
+					nullptr,
+					AUDIO_CHANNELS_OUT,
+					aac_frame->nb_samples,
+					AUDIO_SAMPLE_FORMAT_OUT,
+					0);
+				if (ret < 0)
+					die("Could not allocate samples", ret);
+			}
 
-				auto ret = swr_convert(
+			auto outSamples = swr_convert(
+				encoder_context->audio_converter_context,
+				// output
+				nullptr, 0,
+				// input
+				const_cast<const uint8_t**>(wav_frame->data), wav_frame->nb_samples);
+			if (outSamples < 0)
+				die("Could not convert", outSamples);
+
+			for (;;)
+			{
+				outSamples = swr_get_out_samples(encoder_context->audio_converter_context, 0);
+				if (outSamples < encoder_context->audio_codec_context->frame_size * encoder_context->audio_codec_context->channels)
+					break;
+
+				swr_convert(
 					encoder_context->audio_converter_context,
 					// output
-					&aac_buffer, aac_frame->nb_samples,
+					&convertedData, aac_frame->nb_samples,
 					// input
-					const_cast<const uint8_t**>(&audio_stream_temp), aac_frame->nb_samples);
-				if (ret < 0)
-					die("Cannot convert audio", ret);
+					nullptr, 0);
 
-				const auto aac_buffer_size = av_samples_get_buffer_size(
+				const auto buffer_size = av_samples_get_buffer_size(
 					nullptr,
 					encoder_context->audio_codec_context->channels,
 					aac_frame->nb_samples,
 					encoder_context->audio_codec_context->sample_fmt,
-					1);
+					0);
+				if (buffer_size < 0)
+					die("Invalid buffer size", buffer_size);
 
 				ret = avcodec_fill_audio_frame(
 					aac_frame,
 					encoder_context->audio_codec_context->channels,
 					encoder_context->audio_codec_context->sample_fmt,
-					aac_buffer,
-					aac_buffer_size,
-					1); //no-alignment
+					convertedData,
+					buffer_size,
+					0);
 				if (ret < 0)
-					die("Cannot fill audio frame", ret);
+					die("Could not fill frame", ret);
 
 				av_init_packet(&audio_packet);
+				audio_packet.data = nullptr;
+				audio_packet.size = 0;
 
-				ret = avcodec_send_frame(encoder_context->audio_codec_context, aac_frame);
+				int frameFinished;
+				ret = avcodec_encode_audio2(encoder_context->audio_codec_context, &audio_packet, aac_frame, &frameFinished);
 				if (ret < 0)
-					die("Cannot encode audio frame", ret);
+					die("Error encoding audio frame", ret);
 
-				got_packet_ptr = avcodec_receive_packet(encoder_context->audio_codec_context, &audio_packet) == 0;
-
-				if (got_packet_ptr)
+				if (frameFinished)
 				{
 					audio_packet.stream_index = 1;
 
-					ret = av_interleaved_write_frame(encoder_context->format_context, &audio_packet);
+					ret = av_interleaved_write_frame(encoder_context->muxer_context, &audio_packet);
 					if (ret < 0)
 						die("Error while writing audio frame", ret);
 				}
