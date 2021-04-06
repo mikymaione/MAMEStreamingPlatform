7d0e324ff3e83e99abf14233a3cdbfafdb491fd0
Michele Maione <mikymaione@hotmail.it>
2021-04-06 14:23:05 +0200

    ripristinati originali

--- src/frontend/mame/clifront.h
+++ src/frontend/mame/clifront.h
@@ -2,17 +2,15 @@
 // copyright-holders:Aaron Giles
 /***************************************************************************
 
-	clifront.h
+    clifront.h
 
-	Command-line interface frontend for MAME.
+    Command-line interface frontend for MAME.
 
 ***************************************************************************/
 #ifndef MAME_FRONTEND_CLIFRONT_H
 #define MAME_FRONTEND_CLIFRONT_H
 
 #pragma once
-
-#include <memory>
 
 #include "emuopts.h"
 
@@ -32,57 +30,56 @@ class cli_frontend
 
 public:
 	// construction/destruction
-	cli_frontend(emu_options& options, osd_interface& osd);
+	cli_frontend(emu_options &options, osd_interface &osd);
 	~cli_frontend();
 
 	// execute based on the incoming argc/argv
-	int execute(std::vector<std::string>& args);
+	int execute(std::vector<std::string> &args);
 
 private:
 	struct info_command_struct
 	{
-		const char* option;
+		const char *option;
 		int min_args;
 		int max_args;
-		void (cli_frontend::* function)(const std::vector<std::string>& args);
-		const char* usage;
+		void (cli_frontend::*function)(const std::vector<std::string> &args);
+		const char *usage;
 	};
 
 	// commands
-	void listxml(const std::vector<std::string>& args);
-	void listfull(const std::vector<std::string>& args);
-	void listsource(const std::vector<std::string>& args);
-	void listclones(const std::vector<std::string>& args);
-	void listbrothers(const std::vector<std::string>& args);
-	void listcrc(const std::vector<std::string>& args);
-	void listroms(const std::vector<std::string>& args);
-	void listsamples(const std::vector<std::string>& args);
-	void listdevices(const std::vector<std::string>& args);
-	void listslots(const std::vector<std::string>& args);
-	void listmedia(const std::vector<std::string>& args);
-	void listsoftware(const std::vector<std::string>& args);
-	void verifysoftware(const std::vector<std::string>& args);
-	void verifyroms(const std::vector<std::string>& args);
-	void verifysamples(const std::vector<std::string>& args);
-	void romident(const std::vector<std::string>& args);
-	void getsoftlist(const std::vector<std::string>& args);
-	void verifysoftlist(const std::vector<std::string>& args);
-	void streamingserver(const std::vector<std::string>& args);
-	void version(const std::vector<std::string>& args);
+	void listxml(const std::vector<std::string> &args);
+	void listfull(const std::vector<std::string> &args);
+	void listsource(const std::vector<std::string> &args);
+	void listclones(const std::vector<std::string> &args);
+	void listbrothers(const std::vector<std::string> &args);
+	void listcrc(const std::vector<std::string> &args);
+	void listroms(const std::vector<std::string> &args);
+	void listsamples(const std::vector<std::string> &args);
+	void listdevices(const std::vector<std::string> &args);
+	void listslots(const std::vector<std::string> &args);
+	void listmedia(const std::vector<std::string> &args);
+	void listsoftware(const std::vector<std::string> &args);
+	void verifysoftware(const std::vector<std::string> &args);
+	void verifyroms(const std::vector<std::string> &args);
+	void verifysamples(const std::vector<std::string> &args);
+	void romident(const std::vector<std::string> &args);
+	void getsoftlist(const std::vector<std::string> &args);
+	void verifysoftlist(const std::vector<std::string> &args);
+	void version(const std::vector<std::string> &args);
 
 	// internal helpers
-	template <typename T, typename U> void apply_action(const std::vector<std::string>& args, T&& drvact, U&& devact);
-	template <typename T> void apply_device_action(const std::vector<std::string>& args, T&& action);
+	template <typename T, typename U> void apply_action(const std::vector<std::string> &args, T &&drvact, U &&devact);
+	template <typename T> void apply_device_action(const std::vector<std::string> &args, T &&action);
 	void execute_commands(std::string_view exename);
 	void display_help(std::string_view exename);
-	void output_single_softlist(std::ostream& out, software_list_device& swlist);
-	void start_execution(mame_machine_manager* manager, const std::vector<std::string>& args);
-	static const info_command_struct* find_command(const std::string& s);
+	void output_single_softlist(std::ostream &out, software_list_device &swlist);
+	void start_execution(mame_machine_manager *manager, const std::vector<std::string> &args);
+	static const info_command_struct *find_command(const std::string &s);
 
 	// internal state
-	emu_options& m_options;
-	osd_interface& m_osd;
-	int m_result;
+	emu_options &       m_options;
+	osd_interface &     m_osd;
+	int                 m_result;
 };
 
 #endif  // MAME_FRONTEND_CLIFRONT_H
