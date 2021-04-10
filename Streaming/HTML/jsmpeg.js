8a33c3b672fcb39c9ce57171ee6ff0a8d94c6b15
mikymaione <mikymaione@hotmail.it>
2021-04-10 15:21:58 +0200

    gestione errore html

--- Streaming/HTML/jsmpeg.js
+++ Streaming/HTML/jsmpeg.js
@@ -985,13 +985,42 @@ JSMpeg.Source.WebSocket = function ()
 	WSSource.prototype.onError = function (event)
 	{
 		console.error("WebSocket error observed:", event);
-		alert("The connection failed!");
 	}
 
-	WSSource.prototype.onClose = function ()
+	WSSource.prototype.onClose = function (event)
 	{
 		console.log('The connection has been closed successfully.');
-		alert("Your browser has lost the connection!");
+
+		let specificStatusCodeMappings = {
+			'1000': 'Normal Closure',
+			'1001': 'Going Away',
+			'1002': 'Protocol Error',
+			'1003': 'Unsupported Data',
+			'1004': '(For future)',
+			'1005': 'No Status Received',
+			'1006': 'Abnormal Closure',
+			'1007': 'Invalid frame payload data',
+			'1008': 'Policy Violation',
+			'1009': 'Message too big',
+			'1010': 'Missing Extension',
+			'1011': 'Internal Error',
+			'1012': 'Service Restart',
+			'1013': 'Try Again Later',
+			'1014': 'Bad Gateway',
+			'1015': 'TLS Handshake'
+		};
+
+		let code = event.code;
+
+		if (typeof (specificStatusCodeMappings[code]) !== 'undefined')
+			code = specificStatusCodeMappings[code];
+
+		let msg = "Your browser has lost the connection! Code: " + code;
+
+		if (event.reason)
+			msg = msg + "; Reason: " + event.reason;
+
+		alert(msg);
 	};
 
 	WSSource.prototype.onMessage = function (ev)
