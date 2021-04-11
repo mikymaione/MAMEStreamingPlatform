b522b78a2326930b7ecb3be03eaf3372f09bf2fc
Michele Maione <mikymaione@hotmail.it>
2021-04-11 19:23:06 +0200

    file bat

--- esegui.bat
+++ esegui.bat
@@ -1,4 +1,3 @@
 setx SDL_VIDEODRIVER windows
 setx SDL_AUDIODRIVER directsound
-mame64.exe -streamingserver -window -video accel -sound sdl -resolution 640x480
-pause
+mame64.exe -streamingserver -window -video accel -sound sdl -resolution 640x480@60
