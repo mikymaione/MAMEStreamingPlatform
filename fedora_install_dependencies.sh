7ec9cf239063fd23bd3cb58d753d9a3a2ccd3929
Michele Maione <michele.maione@localhost.localdomain>
2021-04-19 15:12:54 +0200

    aggiunti script installazione fedora

---
+++ fedora_install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+#Add Fedora free packages repo.
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+#Add Fedora non-free packages repo.
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#Add Fedora develop package
+sudo dnf install clang SDL2-devel SDL2_ttf-devel libXi-devel libXinerama-devel qt5-qtbase-devel qt5-qttools expat-devel fontconfig-devel alsa-lib-devel
+#Add Fedora multimedia package
+sudo dnf install ffmpeg-libs portaudio-devel portmidi-devel utf8proc-devel
