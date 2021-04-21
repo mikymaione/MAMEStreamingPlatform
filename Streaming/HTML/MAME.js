b48b500721d583edd09fc2b53a2aac455b06b6df
Michele Maione <mikymaione@hotmail.it>
2021-04-21 19:46:48 +0200

    Web APIs

--- Streaming/HTML/MAME.js
+++ Streaming/HTML/MAME.js
@@ -8,8 +8,8 @@ copyright-holders:Michele Maione
 ============================================================
 */
 
-const uri = "ws://82.48.166.10:8888";
-//const uri = "ws://localhost:8888";
+//const uri = "ws://80.183.63.209:8888";
+const uri = "ws://192.168.1.8:8888";
 
 let keypress_Listener;
 
