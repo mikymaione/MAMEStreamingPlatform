b2fb119d6a86099da2e440c73a2e041f1bb0d80a
Michele Maione <mikymaione@hotmail.it>
2021-06-15 12:02:20 +0200

    correzioni

--- fedora_install_dependencies.sh
+++ fedora_install_dependencies.sh
@@ -7,4 +7,4 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 #Add Fedora develop package
 sudo dnf install clang SDL2-devel SDL2_ttf-devel libXi-devel libXinerama-devel qt5-qtbase-devel qt5-qttools expat-devel fontconfig-devel alsa-lib-devel
 #Add Fedora develop package 3rdparty
-sudo dnf install ffmpeg-devel ffmpeg-libs portaudio-devel portmidi-devel utf8proc-devel pugixml-devel flac-devel sqlite-devel
+sudo dnf install ffmpeg-devel ffmpeg-libs portaudio-devel portmidi-devel utf8proc-devel pugixml-devel flac-devel sqlite-devel glm-devel rapidjson-devel
