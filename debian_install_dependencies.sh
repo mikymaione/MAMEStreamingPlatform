6dc168cdef4a2ab554294af84d4b75cfbb998b57
Michele Maione <mikymaione@hotmail.it>
2024-12-28 16:49:42 +0100

    build: Debian dependecies

---
+++ debian_install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+# Aggiungi i pacchetti di sviluppo per Debian
+sudo apt install clang libsdl2-dev libsdl2-ttf-dev libxi-dev libxinerama-dev qtbase5-dev qttools5-dev-tools libexpat1-dev libfontconfig1-dev libasound2-dev libavcodec-dev libavformat-dev libavutil-dev portaudio19-dev libportmidi-dev libutf8proc-dev libpugixml-dev libflac-dev libsqlite3-dev libglm-dev rapidjson-dev
