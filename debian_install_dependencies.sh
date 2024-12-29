1584e3e990126fd46088cfc103dc8d9eca5ffa6c
Michele Maione <mikymaione@hotmail.it>
2024-12-29 10:21:50 +0100

    build: Debian dependencies

--- debian_install_dependencies.sh
+++ debian_install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 # Aggiungi i pacchetti di sviluppo per Debian
-sudo apt install clang libsdl2-dev libsdl2-ttf-dev libxi-dev libxinerama-dev qtbase5-dev qttools5-dev-tools libexpat1-dev libfontconfig1-dev libasound2-dev libavcodec-dev libavformat-dev libavutil-dev portaudio19-dev libportmidi-dev libutf8proc-dev libpugixml-dev libflac-dev libsqlite3-dev libglm-dev rapidjson-dev
+sudo apt install build-essential libasio-dev libtool autoconf automake make clang python3 python-is-python3 libsdl2-dev libsdl2-ttf-dev libxi-dev libxinerama-dev qtbase5-dev qttools5-dev-tools libexpat1-dev libfontconfig1-dev libasound2-dev libavcodec-dev libavformat-dev libavutil-dev portaudio19-dev libportmidi-dev libutf8proc-dev libpugixml-dev libflac-dev libsqlite3-dev libglm-dev rapidjson-dev libavdevice-dev libswscale-dev
